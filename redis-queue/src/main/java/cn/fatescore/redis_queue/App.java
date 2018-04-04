17e429a82bb3dd36fd7f2b7fea527b79e12ae0b0
hc <hc@hc-vbox>
2018-04-04 11:36:37 +0800

    create projact

---
+++ redis-queue/src/main/java/cn/fatescore/redis_queue/App.java
@@ -0,0 +1,13 @@
+package cn.fatescore.redis_queue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
